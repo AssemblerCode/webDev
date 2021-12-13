02c4e1f7db8628a79aaf29ba429a9a6148027827
AssemblerCode <18205773228@163.com>
2021-12-13 22:24:58 +0800

    提交代码

--- servlet/src/main/java/com/rongke/serv/MyServlet.java
+++ servlet/src/main/java/com/rongke/serv/MyServlet.java
@@ -3,16 +3,7 @@ package com.rongke.serv;
 import javax.servlet.*;
 import java.io.IOException;
 
-/**
- * servlet的继承体系
- */
 public class MyServlet implements Servlet {
-    /**
-     * 该方法用来在tomcat在启动时对该servlet进行初始化用的
-     *
-     * @param config
-     * @throws ServletException
-     */
     @Override
     public void init(ServletConfig config) throws ServletException {
 
@@ -23,16 +14,9 @@ public class MyServlet implements Servlet {
         return null;
     }
 
-    /**
-     * servlet处理请求的方法
-     *
-     * @param req
-     * @param res
-     * @throws ServletException
-     * @throws IOException
-     */
     @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        System.out.println(System.currentTimeMillis());
 
     }
 
@@ -41,9 +25,6 @@ public class MyServlet implements Servlet {
         return null;
     }
 
-    /**
-     * 该方法是tomcat关闭的时候对servlet进行销毁的时候用的方法
-     */
     @Override
     public void destroy() {
 

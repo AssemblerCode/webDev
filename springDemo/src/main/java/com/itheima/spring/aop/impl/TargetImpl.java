a32bb3da118005b5118fdd969d61c8fb04e496c0
AssemblerCode <18205773228@163.com>
2022-03-21 01:53:58 +0800

    学习springaop

--- springDemo/src/main/java/com/itheima/spring/aop/impl/TargetImpl.java
+++ springDemo/src/main/java/com/itheima/spring/aop/impl/TargetImpl.java
@@ -1,15 +1,12 @@
 package com.itheima.spring.aop.impl;
 
 import com.itheima.spring.aop.proxy.TargetInterface;
-import org.springframework.stereotype.Component;
 
-@Component
 public class TargetImpl implements TargetInterface
 {
     @Override
     public void save() {
         System.out.println("save running......");
 
-//        int res = 1 / 0;
     }
 }
